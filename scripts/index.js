2c3b102967177f69fc4ace241591b71886312528
karinana <karina.christa.na@gmail.com>
2023-06-25 13:28:28 -0700

    added close and open JS for modal

--- scripts/index.js
+++ scripts/index.js
@@ -25,4 +25,15 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal__opened");
+});
+
+const modalCloseButton = document.querySelector("#modal-button-close");
+
+modalCloseButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal__opened");
+});
