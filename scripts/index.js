4c3d70d8cc60d8eeda32cde1f9afb9e5812761ee
karinana <karina.christa.na@gmail.com>
2023-06-26 16:40:13 -0700

    feat js to toggle modal

--- scripts/index.js
+++ scripts/index.js
@@ -27,13 +27,50 @@ const initialCards = [
 
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
+const modalCloseButton = document.querySelector("#modal-button-close");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+const profileNameInput = document.querySelector("#name-modal-input");
+const profileDescriptionInput = document.querySelector(
+  "#description-modal-input"
+);
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const elementTemplate =
+  document.querySelector("#element-template").content.firstElementChild;
+const elementCardList = document.querySelector(".elements__cards");
+
+function closePopup() {
+  profileEditModal.classList.remove("modal_opened");
+}
+
+function handleProfileEditSubmit(event) {
+  event.preventDefault();
+  profileName.textContent = profileNameInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopup();
+}
+
+function getCardElement(elementData) {
+  const cardElement = elementTemplate.cloneNode(true);
+  const elementImage = cardElement.querySelector(".elements__image");
+  const elementTitle = cardElement.querySelector(".elements__title");
+
+  elementTitle.textContent = elementData.name;
+  elementTitle.alt = elementData.name;
+  elementImage.src = elementData.link;
+  return cardElement;
+}
 
 profileEditButton.addEventListener("click", () => {
-  profileEditModal.classList.add("modal__opened");
+  profileNameInput.value = profileName.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
+  profileEditModal.classList.add("modal_opened");
 });
 
-const modalCloseButton = document.querySelector("#modal-button-close");
+modalCloseButton.addEventListener("click", closePopup);
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
 
-modalCloseButton.addEventListener("click", () => {
-  profileEditModal.classList.remove("modal__opened");
+initialCards.forEach((elementData) => {
+  const cardElement = getCardElement(elementData);
+  elementCardList.append(cardElement);
 });
